3ac41b3e6399a73fd2ef8cd9c5e3941e2d8f1c2f
Muhammad Umer <rustgrammer@gmail.com>
2024-07-15 19:16:14 +0500

    first deployment

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,10 +3,17 @@ const nav = document.querySelector("header nav");
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("show");
-  console.log(document.documentElement.style.overflow);
+  // console.log(document.documentElement.style.overflow);
   if (document.documentElement.style.overflow == "hidden") {
+    hamburger.src = "";
+
+    hamburger.src =
+      "https://img.icons8.com/?size=30&id=8113&format=png&color=a8a8a8";
     document.documentElement.style.overflow = "";
   } else {
+    hamburger.src = "";
+    hamburger.src =
+      "https://img.icons8.com/?size=32&id=71200&format=png&color=a8a8a8";
     document.documentElement.style.overflow = "hidden";
   }
 });
